3bdfb92ce84031d0476bb0fbdebcfee31f7ddbc8
leonardoffirmino <firmino750@gmail.com>
2023-05-17 23:49:30 -0300

    add color page

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <div className="text-2xl font-bold">Oi</div>
+  return (
+    <div className="h-screen bg-zinc-950 p-6 text-zinc-50">
+      <h1 className="text-4xl font-bold">Sua cápsula do tempo</h1>
+    </div>
+  )
 }
